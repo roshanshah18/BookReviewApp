09a2b421f6f6f449fcf5b6cdd45d9e0478ca0b3e
roshanshah2004 <roshanshah.skillprompt.com>
2024-12-15 11:50:23 +0545

    change in cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -21,7 +21,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", ""],
+    origin: [
+      "http://localhost:5173",
+      "https://book-review-cgdgj0qjf-roshanshah18s-projects.vercel.app",
+    ],
     credentials: true,
   })
 );
