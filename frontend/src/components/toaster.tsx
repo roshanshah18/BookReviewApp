91723e4cfb1ccf0f528d57bcec31d4fa9c01d690
roshanshah18 <rs6927921@gmail.com>
2024-12-16 11:08:22 +0545

    again toaster position change

--- frontend/src/components/toaster.tsx
+++ frontend/src/components/toaster.tsx
@@ -3,7 +3,7 @@ import { toast } from "react-hot-toast";
 export function errorToast(message: string) {
   toast(message, {
     duration: 2000,
-    position: "top-right",
+    position: "bottom-center",
     style: {
       background: "#ef4444",
       color: "#fff",
@@ -14,7 +14,7 @@ export function errorToast(message: string) {
 export function successToast(message: string) {
   toast(message, {
     duration: 2000,
-    position: "top-right",
+    position: "bottom-center",
     style: {
       background: "#22c55e",
       color: "#fff",
